178c73660d88859a8a71946971f0c3831cfb52a2
GFORTI <GFORTI@N206INSTR.labs.neit.edu>
2013-10-23 20:12:42 -0400

    form lab complete

--- week3/index.php
+++ week3/index.php
@@ -14,14 +14,39 @@ and open the template in the editor.
         // put your code here
         
         //$_POST
+        
+        //echo $_POST["fullname"];
+        
+        //print_r($_POST);
+        
+            $fullname = "";
+            $email = "";
+            $comments = "";
+            
+            if ( count($_POST) ) {
+               
+                 if ( array_key_exists("fullname", $_POST) ) {
+                     $fullname = $_POST["fullname"];
+                 }
+                 
+                 if ( array_key_exists("email", $_POST) ) {
+                     $email = $_POST["email"];
+                 }
+                 
+                 if ( array_key_exists("comments", $_POST) ) {
+                     $comments = $_POST["comments"];
+                 }
+
+            }
         ?>
         
         
         <form name="mainform" action="index.php" method="post">
             
-            Full name: <input type="text" name="fullname" value="" /> <br />
-            
-            <input type="submit" value="Submit" />
+            Full name: <input type="text" name="fullname" value="<?php echo $fullname; ?>" /> <br />
+            Email: <input type="text" name="email" value="<?php echo $email; ?>" /> <br />
+            Comments: <br /> <textarea cols="20" rows="5" name="comments"><?php echo $comments; ?></textarea> <br />
+            <input name="submit" type="submit" value="Submit" />
             
         </form>
         
