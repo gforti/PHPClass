fca064777e97b8b6167941f9856620b785783406
GFORTI <GFORTI@N206INSTR.labs.neit.edu>
2013-11-06 20:15:12 -0500

    week5-2 guestbook

---
+++ week5-2/guestbook.php
@@ -0,0 +1,25 @@
+<?php include 'dependency.php'; ?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My Guestbook</title>
+    </head>
+    <body>
+        <?php
+        // put your code here        
+        Login::confirmAccess();
+        
+        
+        $gb = new Guestbook();
+        $gb->displayGuestbook();
+        
+        ?>
+       
+    </body>
+</html>
