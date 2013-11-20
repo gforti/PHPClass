7f8fa44266bfad8b4ee7a35718dc6cc9a9c017ac
GFORTI <GFORTI@N206INSTR.labs.neit.edu>
2013-11-20 17:44:40 -0500

    contact form demo

---
+++ week1/form.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+            if ( count($_POST) ) {
+                
+                $email = filter_input(INPUT_POST, "email");
+                $comments = filter_input(INPUT_POST, "comments");
+               
+                
+                // validation checks
+                
+                mail("me@gmail.com", "Post from site", $comments." contact: $email");
+                
+            }
+        ?>
+        
+        
+        <form action="#" method="post">
+            
+           email <input name="email" type="text"> <br />
+           comments <input name="comments" type="text"> <br />
+            
+           <input type="submit" value="submit" />
+            
+        </form>
+        
+    </body>
+</html>
